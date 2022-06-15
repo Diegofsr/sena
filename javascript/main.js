aa6a32e9bbcd3847e0a279a7d59bfaa3fd53b2a0
Diego <diegofsantosr@gmail.com>
2022-06-15 16:40:33 -0500

    Actualizando codigo final

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+document.getElementById("sidebar-container").style.width = "250px";
+document.getElementById("main").style.marginLeft = "250px";
+document.getElementById("main").style.display = "none";
+}
+
+function closeNav() {
+document.getElementById("sidebar-container").style.width = "0";
+document.getElementById("main").style.marginLeft = "0";
+document.getElementById("main").style.display = "block";
+}
