82b8494eae233bcb4d292d31d73a0f195ca6e112
Diego <diegofsantosr@gmail.com>
2022-07-01 08:26:45 -0500

    maquetacion del codigo

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,12 @@
 // funciones para abrir y cerrar el sidebar
 function openNav() {
-document.getElementById("sidebar-container").style.width = "250px";
-document.getElementById("main").style.marginLeft = "250px";
-document.getElementById("main").style.display = "none";
+    document.getElementById("sidebar-container").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main").style.display = "none";
 }
 
 function closeNav() {
-document.getElementById("sidebar-container").style.width = "0";
-document.getElementById("main").style.marginLeft = "0";
-document.getElementById("main").style.display = "block";
+    document.getElementById("sidebar-container").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main").style.display = "block";
 }
